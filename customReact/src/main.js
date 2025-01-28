4b49800c15af5396b5ed6b2ca3b5943255e3e9e4
Arslan Shaukat <arslansb@outlook.com>
2025-01-28 22:03:44 +0500

    custom react render

--- customReact/src/main.js
+++ customReact/src/main.js
@@ -1,1 +1,29 @@
 
+
+function customRender(reactElement, mainContainer){
+
+    let element = document.createElement(reactElement.type);
+    element.innerHTML= reactElement.children;
+
+    for (const prop in reactElement.props){
+        if (prop === 'children') continue;
+        element.setAttribute(prop,reactElement.props[prop])
+    }
+    mainContainer.appendChild(element);
+
+}
+
+const reactElement={
+    type: 'a',
+    props: {
+        href: 'https://www.arslanshaukat.online/',
+        'target': '_blank',
+    },
+    children: 'Vist Portfolio'
+}
+
+
+
+let mainContainer= document.querySelector('#root');
+
+customRender(reactElement,mainContainer);
